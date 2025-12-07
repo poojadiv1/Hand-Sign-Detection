d013ff0978e64b8496fa89ebe034898f8de8f812
pooja <pooja05092005@gmail.com>
2025-12-07 16:42:37 +0530

    commit 2

---
+++ DataCollection.py
@@ -0,0 +1,25 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+imgSize = 300
+offset = 20
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize,imgSize,3), np.uint8)*255
+
+        imgCrop = img[y-offset:y + h+offset, x-offset:x + w+offset]
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
